085ea9fced4f281c3c3a1207fb34f013a426d7aa
ttom921 <ttom921@hotmail.com>
2019-11-27 10:56:32 +0800

    更新

--- HelloAngular/src/app/lession2-10/child-one2t10/child-one2t10.component.ts
+++ HelloAngular/src/app/lession2-10/child-one2t10/child-one2t10.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, ContentChild, AfterContentInit } from '@angular/core';
+import { Component, OnInit, ContentChild, AfterContentInit, ContentChildren, QueryList } from '@angular/core';
 import { ChildTwo2t10Component } from '../child-two2t10/child-two2t10.component';
 
 @Component({
@@ -9,15 +9,21 @@ import { ChildTwo2t10Component } from '../child-two2t10/child-two2t10.component'
 export class ChildOne2t10Component implements OnInit, AfterContentInit {
 
   //注解的寫法
-  @ContentChild(ChildTwo2t10Component) childtwo: ChildTwo2t10Component;
+  //@ContentChild(ChildTwo2t10Component) childtwo: ChildTwo2t10Component;
+  //這時候不是單個組，是一個列表QueryList
+  @ContentChildren(ChildTwo2t10Component) childrenTwo: QueryList;
   constructor() { }
 
   ngOnInit() {
   }
   //在ngAfterContentInit鉤子里面訪問被投影進來的組件
   ngAfterContentInit(): void {
-    console.log(this.childtwo);
+    //console.log(this.childtwo);
     //這里還可以訪問 this.childtwo的public型方法，監聽 this.childtwo所派發出來的事件
-  }
 
+    //遍歷列表
+    this.childrenTwo.forEach(item => {
+      console.log(item);
+    });
+  }
 }
