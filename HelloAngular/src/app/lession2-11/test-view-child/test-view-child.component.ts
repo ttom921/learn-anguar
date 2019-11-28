21083afb4c2079d782a0fc9a1049764c5e1a2ec0
ttom921 <ttom921@hotmail.com>
2019-11-28 09:31:06 +0800

    更新代碼

--- HelloAngular/src/app/lession2-11/test-view-child/test-view-child.component.ts
+++ HelloAngular/src/app/lession2-11/test-view-child/test-view-child.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, ViewChild, AfterViewInit } from '@angular/core';
+import { Component, OnInit, ViewChild, AfterViewInit, ViewChildren, QueryList } from '@angular/core';
 import { ChilOne2a11Component } from '../chil-one2a11/chil-one2a11.component';
 
 @Component({
@@ -9,18 +9,26 @@ import { ChilOne2a11Component } from '../chil-one2a11/chil-one2a11.component';
 export class TestViewChildComponent implements OnInit, AfterViewInit {
 
 
-  @ViewChild(ChilOne2a11Component, { static: true }) childOne: ChilOne2a11Component;
+  //@ViewChild(ChilOne2a11Component, { static: true }) childOne: ChilOne2a11Component;
+  @ViewChildren(ChilOne2a11Component) children: QueryList<ChilOne2a11Component>;
   constructor() { }
 
   ngOnInit() {
   }
   //在ngAfterViewInit 這個鉤子里面可以直接訪問子組件
   ngAfterViewInit(): void {
-    console.log(this.childOne);
-    //用代碼的方式訂閱子組件上的事件
-    this.childOne.helloEvent.subscribe(param => {
-      console.log(param);
-      console.log(this.childOne.title);
+    // console.log(this.childOne);
+    // //用代碼的方式訂閱子組件上的事件
+    // this.childOne.helloEvent.subscribe(param => {
+    //   console.log(param);
+    //   console.log(this.childOne.title);
+    // });
+    this.children.forEach(item => {
+      //console.log(item);
+      //動態監聽子組件的事件
+      item.helloEvent.subscribe(data => {
+        console.log(data);
+      });
     });
   }
 
