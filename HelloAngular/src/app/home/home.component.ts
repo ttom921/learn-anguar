af34992d4b3552538e1769e3fedf41b43dded2d2
ttom921 <ttom921@hotmail.com>
2019-12-12 09:31:11 +0800

    處理路由事件

--- HelloAngular/src/app/home/home.component.ts
+++ HelloAngular/src/app/home/home.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router, NavigationStart } from '@angular/router';
 
 @Component({
   selector: 'app-home',
@@ -7,9 +8,16 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
 
-  constructor() { }
+  constructor(private router: Router) {
+    this.router.events.subscribe((event) => {
+      console.log(event);
+      //可以爭instanceof來判斷事件的類型，然後去做你想要做的事情
+      console.log(event instanceof NavigationStart);
+    });
+  }
 
   ngOnInit() {
+
   }
 
 }
