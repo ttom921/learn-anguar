ac67a8ea6df49f9c2a254fce7b013efd16e8081a
ttom921 <ttom921@hotmail.com>
2020-01-09 09:53:11 +0800

    更新程式

--- HelloAngular/src/app/app.component.spec.ts
+++ HelloAngular/src/app/app.component.spec.ts
@@ -1,35 +1,52 @@
-// import { TestBed, async } from '@angular/core/testing';
-// import { RouterTestingModule } from '@angular/router/testing';
-// import { AppComponent } from './app.component';
+import { TestBed, async } from '@angular/core/testing';
+import { RouterTestingModule } from '@angular/router/testing';
+import { AppComponent } from './app.component';
+import { UnitTestComponent } from './unit-test/unit-test.component';
+import { LanguageService } from './service/language.service';
+import { TranslateService, TranslateStore, TranslateLoader, TranslateCompiler, TranslateModule } from '@ngx-translate/core';
+import { HttpLoaderFactory } from './app.module';
+import { HttpClient, HttpClientModule } from '@angular/common/http';
 
-// describe('AppComponent', () => {
-//   beforeEach(async(() => {
-//     TestBed.configureTestingModule({
-//       imports: [
-//         RouterTestingModule
-//       ],
-//       declarations: [
-//         AppComponent
-//       ],
-//     }).compileComponents();
-//   }));
+xdescribe('AppComponent', () => {
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      imports: [
+        RouterTestingModule,
+        HttpClientModule,
+        TranslateModule.forRoot({
+          loader: {
+            provide: TranslateLoader,
+            useFactory: HttpLoaderFactory,
+            deps: [HttpClient]
+          }
+        }),
+      ],
+      declarations: [
+        AppComponent,
+        UnitTestComponent
+      ],
+      providers: [
+        LanguageService
+      ]
+    }).compileComponents();
+  }));
 
-//   it('should create the app', () => {
-//     const fixture = TestBed.createComponent(AppComponent);
-//     const app = fixture.debugElement.componentInstance;
-//     expect(app).toBeTruthy();
-//   });
+  it('should create the app', () => {
+    const fixture = TestBed.createComponent(AppComponent);
+    const app = fixture.debugElement.componentInstance;
+    expect(app).toBeTruthy();
+  });
 
-//   it(`should have as title 'HelloAngular'`, () => {
-//     const fixture = TestBed.createComponent(AppComponent);
-//     const app = fixture.debugElement.componentInstance;
-//     expect(app.title).toEqual('HelloAngular');
-//   });
+  // it(`should have as title 'HelloAngular'`, () => {
+  //   const fixture = TestBed.createComponent(AppComponent);
+  //   const app = fixture.debugElement.componentInstance;
+  //   expect(app.title).toEqual('HelloAngular');
+  // });
 
-//   it('should render title in a h1 tag', () => {
-//     const fixture = TestBed.createComponent(AppComponent);
-//     fixture.detectChanges();
-//     const compiled = fixture.debugElement.nativeElement;
-//     expect(compiled.querySelector('h1').textContent).toContain('Welcome to HelloAngular!');
-//   });
-// });
+  // it('should render title in a h1 tag', () => {
+  //   const fixture = TestBed.createComponent(AppComponent);
+  //   fixture.detectChanges();
+  //   const compiled = fixture.debugElement.nativeElement;
+  //   expect(compiled.querySelector('h1').textContent).toContain('Welcome to HelloAngular!');
+  // });
+});
