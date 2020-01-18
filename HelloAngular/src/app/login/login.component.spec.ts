d6e56aaae30a2fb301034e9591945b7fa7b75156
ttom921 <ttom921@hotmail.com>
2020-01-18 10:32:48 +0800

    更新程試碼

--- HelloAngular/src/app/login/login.component.spec.ts
+++ HelloAngular/src/app/login/login.component.spec.ts
@@ -43,3 +43,109 @@ describe('LoginComponent', () => {
   });
 
 });
+//--- Mocking with fake Class---
+class MockAuthService {
+  authenticated = false;
+  isAuthenticated(): boolean {
+    return this.authenticated;
+  }
+}
+describe('LoginCompoenet with mock', () => {
+  let component: LoginComponent;
+  let fixture: ComponentFixture<LoginComponent>;
+  let service: MockAuthService;
+  beforeEach(async () => {
+    TestBed.configureTestingModule({
+      declarations: [LoginComponent],
+      imports: [ReactiveFormsModule],
+      providers: [MockAuthService]
+    })
+      .compileComponents();
+  });
+  beforeEach(() => {
+    fixture = TestBed.createComponent(LoginComponent);
+    component = fixture.componentInstance;
+    service = TestBed.get(MockAuthService);
+    fixture.detectChanges();
+  });
+  it('should be create', () => {
+    expect(component).toBeTruthy();
+  });
+  it('should login failed when user is not authenticated', () => {
+    service.authenticated = false;
+    expect(service.isAuthenticated()).toBeFalsy();
+  });
+  it('should login success when user is authenticatrd', () => {
+    service.authenticated = true;
+    expect(service.isAuthenticated()).toBeTruthy();
+  });
+});
+//--- Mocking by overriding functions----
+class MockAuthServiceExt extends DemoService {
+  authenticated = false;
+  isAuthenticated() {
+    return this.authenticated;
+  }
+}
+describe('LoginCompoenet with mock extends', () => {
+  let component: LoginComponent;
+  let fixture: ComponentFixture<LoginComponent>;
+  let service: MockAuthServiceExt;
+  beforeEach(async () => {
+    TestBed.configureTestingModule({
+      declarations: [LoginComponent],
+      imports: [ReactiveFormsModule],
+      providers: [MockAuthServiceExt]
+    })
+      .compileComponents();
+  });
+  beforeEach(() => {
+    fixture = TestBed.createComponent(LoginComponent);
+    component = fixture.componentInstance;
+    service = TestBed.get(MockAuthServiceExt);
+    fixture.detectChanges();
+  });
+  it('should be create', () => {
+    expect(component).toBeTruthy();
+  });
+  it('should login failed when user is not authenticatrd', () => {
+    service.authenticated = false;
+    expect(service.isAuthenticated()).toBeFalsy();
+  });
+  it('should login success when user is authenticatrd', () => {
+    service.authenticated = true;
+    expect(service.isAuthenticated()).toBeTruthy();
+  });
+});
+//----Mocking with real service with Spy----
+describe('Mocking with real service with Spy', () => {
+  let component: LoginComponent;
+  let fixture: ComponentFixture<LoginComponent>;
+  let service: DemoService;
+  beforeEach(async () => {
+    TestBed.configureTestingModule({
+      declarations: [LoginComponent],
+      imports: [ReactiveFormsModule],
+      providers: [DemoService]
+    })
+      .compileComponents();
+  });
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(LoginComponent);
+    component = fixture.componentInstance;
+    service = TestBed.get(DemoService);
+    fixture.detectChanges();
+  });
+  it('should login success whtn user is authenticated', () => {
+    //SpyOn
+    spyOn(service, 'isAuthenticated').and.returnValue(true);
+    expect(service.isAuthenticated()).toBeTruthy();
+  });
+  it('should login failed when user is no authenticated', () => {
+    //SpyOn
+    spyOn(service, 'isAuthenticated').and.returnValue(false);
+    expect(service.isAuthenticated()).toBeFalsy();
+  });
+
+});
