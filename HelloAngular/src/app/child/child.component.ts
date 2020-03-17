f852dfddadb9191738a10609de79791f9468953f
ttom921 <ttom921@hotmail.com>
2020-03-17 11:22:05 +0800

    更新程式碼

--- HelloAngular/src/app/child/child.component.ts
+++ HelloAngular/src/app/child/child.component.ts
@@ -1,19 +1,19 @@
-import { Component, OnInit, Self } from '@angular/core';
+import { Component, OnInit, Optional, Self } from '@angular/core';
 import { UserListService } from '../user-list/service/user-list.service';
 
 @Component({
   selector: 'app-child',
   templateUrl: './child.component.html',
   styleUrls: ['./child.component.scss'],
-  providers: [UserListService]
 })
 export class ChildComponent implements OnInit {
 
   constructor(
-    @Self() private userListService: UserListService,
+    @Self()
+    @Optional()
+    private userListService: UserListService,
   ) { }
 
   ngOnInit() {
   }
-
 }
--- HelloAngular/src/app/user-list/user-list.component.ts
+++ HelloAngular/src/app/user-list/user-list.component.ts
@@ -3,13 +3,14 @@ import { UserListService } from './service/user-list.service';
 @Component({
   selector: 'user-list',
   templateUrl: './user-list.component.html',
-  styleUrls: ['./user-list.component.scss']
+  styleUrls: ['./user-list.component.scss'],
+  //providers: [UserListService]
 })
 export class UserListComponent implements OnInit {
 
   userList: Array<any>;
   constructor(
-    private userListService: UserListService,
+    //private userListService: UserListService,
   ) { }
 
   ngOnInit() {
