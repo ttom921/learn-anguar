2bc194c14bd25f351a65a092fd6930b3c9744d6b
ttom921 <ttom921@hotmail.com>
2020-03-23 10:44:22 +0800

    更新程式碼

--- HelloAngular/src/app/child/child.component.ts
+++ HelloAngular/src/app/child/child.component.ts
@@ -1,15 +1,14 @@
-import { Component, OnInit, Optional, Self, SkipSelf } from '@angular/core';
+import { Component, OnInit, Optional, Self, SkipSelf, Host } from '@angular/core';
 import { UserListService } from '../user-list/service/user-list.service';
 
 @Component({
   selector: 'app-child',
   templateUrl: './child.component.html',
   styleUrls: ['./child.component.scss'],
-  providers: [UserListService]
 })
 export class ChildComponent implements OnInit {
   constructor(
-    @SkipSelf()
+    @Host()
     @Optional()
     private userListService: UserListService,
   ) { }
